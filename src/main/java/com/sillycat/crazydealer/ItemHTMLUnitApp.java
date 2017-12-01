cf279df3a9801a71d433585872a5ebb6de0fc059
Carl Luo <cluo@jobs2careers.com>
2017-12-01 10:24:50 -0600

    test 2 different way

--- src/main/java/com/sillycat/crazydealer/ItemHTMLUnitApp.java
+++ src/main/java/com/sillycat/crazydealer/ItemHTMLUnitApp.java
@@ -1,5 +1,6 @@
 package com.sillycat.crazydealer;
 
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.MalformedURLException;
 
@@ -11,25 +12,26 @@ import com.gargoylesoftware.htmlunit.BrowserVersion;
 import com.gargoylesoftware.htmlunit.FailingHttpStatusCodeException;
 import com.gargoylesoftware.htmlunit.WebClient;
 import com.gargoylesoftware.htmlunit.WebRequest;
+import com.gargoylesoftware.htmlunit.html.HtmlButton;
+import com.gargoylesoftware.htmlunit.html.HtmlElement;
 import com.gargoylesoftware.htmlunit.html.HtmlEmailInput;
 import com.gargoylesoftware.htmlunit.html.HtmlForm;
 import com.gargoylesoftware.htmlunit.html.HtmlPage;
 import com.gargoylesoftware.htmlunit.html.HtmlPasswordInput;
 import com.gargoylesoftware.htmlunit.html.HtmlSubmitInput;
 
-public class LogonHTMLUnitApp {
+public class ItemHTMLUnitApp {
 
-	private final static Logger logger = LoggerFactory.getLogger(LogonHTMLUnitApp.class);
+	private final static Logger logger = LoggerFactory.getLogger(ItemHTMLUnitApp.class);
 
 	private final static String HOME_URL = "https://www.costco.com/";
-	private final static String LOGIN_URL = "https://www.costco.com/LogonForm";
-	private final static String ITEM_URL = "https://www.costco.com/xbox-one.html";
+	private final static String ITEM_URL = "https://www.costco.com/Xbox-Live-24-Month-Gold-Card.product.100243703.html";
 
 	public static void main(String[] args) {
 		logger.info("Start to init the HTTP Client---------");
 		// BEST_SUPPORTED, CHROME, EDGE, FIREFOX_45, FIREFOX_52,
 		// INTERNET_EXPLORER
-		final WebClient webClient = new WebClient(BrowserVersion.FIREFOX_45);
+		final WebClient webClient = new WebClient(BrowserVersion.CHROME);
 		webClient.getOptions().setThrowExceptionOnScriptError(false);
 		webClient.getOptions().setThrowExceptionOnFailingStatusCode(false);
 		webClient.getOptions().setCssEnabled(true);
@@ -43,35 +45,40 @@ public class LogonHTMLUnitApp {
 		webClient.getOptions().setDownloadImages(true);
 		webClient.getOptions().setGeolocationEnabled(true);
 		webClient.getCookieManager().setCookiesEnabled(true);
-		webClient.setAjaxController(new AjaxController() {
-			private static final long serialVersionUID = -5849069892969682176L;
-
-			public boolean processSynchron(HtmlPage page, WebRequest request, boolean async) {
-				return true;
-			}
-		});
+		// webClient.setAjaxController(new AjaxController() {
+		// private static final long serialVersionUID = -5849069892969682176L;
+		//
+		// public boolean processSynchron(HtmlPage page, WebRequest request,
+		// boolean async) {
+		// return true;
+		// }
+		// });
 
 		try {
+			webClient.getCookieManager().setCookiesEnabled(true);
 			webClient.getPage(HOME_URL);
-			HtmlPage logonPage = webClient.getPage(LOGIN_URL);
+			HtmlPage itemPage = webClient.getPage(ITEM_URL);
+			webClient.getCookieManager().setCookiesEnabled(true);
+			final HtmlForm itemForm = itemPage.getFormByName("OrderItemAddForm");
+			HtmlElement signButton = itemForm.getInputByName("sign-in-to-buy");
+			webClient.getCookieManager().setCookiesEnabled(true);
+			HtmlPage logonPage = signButton.click();
+			webClient.getCookieManager().setCookiesEnabled(true);
 			final HtmlForm form = logonPage.getFormByName("LogonForm");
 			HtmlEmailInput textUserName = form.getInputByName("logonId");
 			textUserName.setText("luohuazju@gmail.com");
 			HtmlPasswordInput txtPwd = form.getInputByName("logonPassword");
 			txtPwd.setText("kaishi117A");
 			HtmlSubmitInput submitInput = form.getInputByValue("Sign In");
+			webClient.getCookieManager().setCookiesEnabled(true);
 			HtmlPage loginAfterPage = submitInput.click();
-
-			logger.trace("*************************************************************************************");
-			logger.trace(loginAfterPage.getWebResponse().getContentAsString());
-			logger.trace("*************************************************************************************");
-			logger.trace("Cookies : " + webClient.getCookieManager().getCookies().toString());
-
-			HtmlPage itemPage = webClient.getPage(ITEM_URL);
-			webClient.waitForBackgroundJavaScript(6000);
-			
+			webClient.getCookieManager().setCookiesEnabled(true);
 			logger.info("*************************************************************************************");
-			logger.info(itemPage.getWebResponse().getContentAsString());
+			logger.info(loginAfterPage.getWebResponse().getContentAsString());
+			byte data[] = loginAfterPage.getWebResponse().getContentAsString().getBytes();
+			FileOutputStream out = new FileOutputStream("/Users/carl/Downloads/picture/index.html");
+			out.write(data);
+			out.close();
 			logger.info("*************************************************************************************");
 			logger.info("Cookies : " + webClient.getCookieManager().getCookies().toString());
 
